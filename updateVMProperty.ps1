222a7ea982784bf51c0f2809cf760dedb10f818d
yogeshyadav81 <yogesh.yadav81@gmail.com>
2019-09-30 00:27:12 +0530

    update

--- updateVMProperty.ps1
+++ updateVMProperty.ps1
@@ -6,16 +6,7 @@
 )
 
    #Stop-AzureRmVM -ResourceGroupName $resourceGroupName -Name $vmName -Force
-   $MyDomain='HCL'
-$MyClearTextUsername='vipul_ku'
-$MyClearTextPassword='Vip!060985'
-
-$MyUsernameDomain=$MyDomain+’\’+$MyClearTextUsername
-
-$SecurePassword=Convertto-SecureString –String $MyClearTextPassword –AsPlainText –force
-
-$MyCredentials=New-object System.Management.Automation.PSCredential $MyUsernameDomain,$SecurePassword
-Connect-AzureRmAccount $MyCredentials
+   
    $vm = Get-AzureRmVM -ResourceGroupName $resourceGroupName -Name $vmName
    $vm.HardwareProfile.VmSize = 'Standard_DS2_v2'#$vmSize
 
