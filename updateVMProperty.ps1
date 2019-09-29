2cab8063acbed909cc5585613d2aca8ca7e281fd
yogeshyadav81 <yogesh.yadav81@gmail.com>
2019-09-29 22:56:17 +0530

    update

--- updateVMProperty.ps1
+++ updateVMProperty.ps1
@@ -7,9 +7,9 @@
 
    #Stop-AzureRmVM -ResourceGroupName $resourceGroupName -Name $vmName -Force
    
-   $vm = Get-AzureRmVM -ResourceGroupName $resourceGroupName -Name $vmName
+   $vm = Get-AzVM -ResourceGroupName $resourceGroupName -Name $vmName
    $vm.HardwareProfile.VmSize = 'Standard_DS2_v2'#$vmSize
 
-   Update-AzureRmVM -ResourceGroupName $resourceGroupName -VM $vm
+   Update-AzVM -ResourceGroupName $resourceGroupName -VM $vm
 
    #Start-AzureRmVM -ResourceGroupName $resourceGroupName -Name $vm.Name
